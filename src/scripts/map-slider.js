28097c11a18bb74ccc0b752e83265d5ac33f7a74
smerch88 <maxarmyk@gmail.com>
2022-11-26 19:59:48 +0200

    save

--- src/scripts/map-slider.js
+++ src/scripts/map-slider.js
@@ -7,6 +7,10 @@ import "swiper/swiper.min.css";
 import "swiper/modules/navigation/navigation.min.css";
 import "swiper/modules/pagination/pagination.min.css";
 
+const regionButtonsRef = document.querySelectorAll(".swiper-slide");
+console.log(regionButtonsRef);
+const oblastRender = document.querySelector(".oblastJS");
+
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   grid: {
@@ -17,9 +21,12 @@ const swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  on: {
+    click(event) {
+      console.log("event.target", this.clickedIndex);
+      swiper.slideTo(this.clickedIndex);
+    },
+  },
 });
 
-const regionButtonsRef = document.querySelectorAll(".swiper-slide");
-console.log(regionButtonsRef);
-
-regionButtonsRef.forEach((element) => console.log(element.dataset));
+const renderOblast = () => {};
